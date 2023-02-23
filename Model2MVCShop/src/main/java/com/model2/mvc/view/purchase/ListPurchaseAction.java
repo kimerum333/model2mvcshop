64a0d62d850c0d2ca5353288e06f1c3332c26164
bitcamp <bitcamp@DESKTOP-RJJ5KHS>
2023-02-23 11:12:39 +0900

    ver1/new:2023.02.23 사용자입장 구매목록조회 만드는중

--- Model2MVCShop/src/main/java/com/model2/mvc/view/purchase/ListPurchaseAction.java
+++ Model2MVCShop/src/main/java/com/model2/mvc/view/purchase/ListPurchaseAction.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpSession;
 
 import com.model2.mvc.common.SearchVO;
 import com.model2.mvc.framework.Action;
+import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.purchase.impl.PurchaseServiceImpl;
 import com.model2.mvc.service.user.vo.UserVO;
 
 public class ListPurchaseAction extends Action {
@@ -21,10 +23,19 @@ public class ListPurchaseAction extends Action {
 		String userId = ((UserVO)session.getAttribute("user")).getUserId();
 		
 		//검색조건인 Search를 확보합니다.
-		SearchVO searchVO = new SearchVO();
-		if(request.getAttribute("searchVO")!=null) {
-			searchVO = (SearchVO)request.getAttribute("searchVO");
-		}
+		//생각해보니까 나중에 만들어도 될 것 같습니다. 한 사람이 구매한 목록을 전부 합쳐봐야 한 페이지에 다 나올테니
+		/*
+		 * SearchVO searchVO = new SearchVO();
+		 * if(request.getAttribute("searchVO")!=null) { searchVO =
+		 * (SearchVO)request.getAttribute("searchVO"); }
+		 */
+		
+		//비즈니스로직을 수행할 서비스를 만듭니다.
+		PurchaseService purchaseService = new PurchaseServiceImpl();
+		//TODO 서비스에 겟 퍼체이스리스트를 선언합니다.
+		purchaseService.getPurchaseList()
+		
+		
 		//TODO 퍼체이스리스트를 마무리하면됩니다.
 		return null;
 	}
